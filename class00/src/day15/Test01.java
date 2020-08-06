de2acc3ec3d3e2a33f71f573bb9934661015617e
class02 <class02@192.168.0.250>
2020-08-06 09:46:45 +0900

    class test

--- class00/src/day15/Test01.java
+++ class00/src/day15/Test01.java
@@ -1,5 +1,10 @@
 package day15;
-
+	
 public class Test01 {
-
+	public Test01() {
+		
+	}
+public static void main(String[] args) {
+	new Test01();
+}
 }
---
+++ class00/src/day16/Test01.java
@@ -0,0 +1,16 @@
+package day16;
+
+import javax.swing.*;
+public class Test01 {
+	public Test01() {
+		
+		String sno = JOptionPane.showInputDialog("정수를입력하세요");
+	}
+	
+	public static void main(String[] args) {
+		new Test01();
+		
+
+	}
+
+}
